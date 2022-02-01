2db55c24452b3bd2f65fbe37ab17902530c03c0d
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2022-02-02 00:15:13 +0300

    Solve Clojure "Run-length-encode" task

---
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -0,0 +1,20 @@
+(ns run-length-encoding)
+
+(defn run-length-encode
+  "encodes a string with run-length-encoding"
+  [plain-text]
+  (let [ [base last-acc] (reduce (fn [ [base acc last] ch]
+          (cond
+            (empty? acc)   [ base (list ch) ch ]
+            (not= last ch) [ (conj base acc) (list ch) ch ]
+            :else          [ base (conj acc ch) ch ] )) [ () () nil ] plain-text)
+      accs (reverse (conj base last-acc))]
+  (reduce (fn [s acc] (str s (let [cnt (count acc)] (if (> cnt 1) cnt "")) (first acc) )  ) "" accs)))
+
+(defn run-length-decode
+  "decodes a run-length-encoded string"
+  [cipher-text]
+  (reduce (fn [ s [ _ cnts ch one-ch ] ]
+            (str s (if (nil? cnts) one-ch
+                      (apply str (repeat (Integer/parseInt cnts) ch)))) ) ""
+          (re-seq #"(\d+)(.)|(.)" cipher-text)))
